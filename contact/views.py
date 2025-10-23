8248e33aad1f6004eae51ce3e559821c56baa491
NellitG <kamaunelson157@gmail.com>
2025-10-23 11:38:32 +0300

    Create contact app with reservation model, serializer, url and views

---
+++ contact/views.py
@@ -0,0 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Contact
+from .serializers import ContactSerializer
+
+@api_view(['POST'])
+def contact_create(request):
+    serializer = ContactSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 'Contact saved successfully'}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
