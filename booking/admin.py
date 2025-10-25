6145e33654324f45b943d2258af0a759ef408b98
NellitG <kamaunelson157@gmail.com>
2025-10-25 12:14:32 +0300

    create admin display for booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reservation as booking
 
 # Register your models here.
+@admin.register(booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'county', 'payment', 'date', 'created_at')
